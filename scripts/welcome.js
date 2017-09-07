fe4f4245a48b55effbbeb69e0c74013d19377b4e
Topher Brown <t.brown@wordstream.com>
2017-09-07 15:47:39 -0400

    add logging

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,6 +1,8 @@
 module.exports = function(robot) {
     robot.enter(function(res) {
+        console.log(`received enter message: ${res}, ${res.message.room}`);
         if (res.message.room === 'bot-test') {
+            console.log('replying to new user');
             res.reply("Welcome to wordstream!");
         }
     });
