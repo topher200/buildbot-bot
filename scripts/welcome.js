eb293332ddebe88b260200a6fcd8547505b0f8c4
Topher Brown <t.brown@wordstream.com>
2017-09-07 16:49:22 -0400

    clean up var names

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -19,18 +19,20 @@ var WELCOME_MESSAGE = 'Welcome to Wordstream!';
 module.exports = function(robot) {
     robot.enter(function(res) {
         var roomName = robot.adapter.client.rtm.dataStore.getChannelGroupOrDMById(res.message.room).name;
-        console.log(`received enter event. room name: ${roomName}, user: ${res.message.user.name}`);
+        var userName = res.message.user.name;
+        console.log(`received enter event. room name: ${roomName}, user: ${userName}`);
         if (ROOM_LIST.includes(roomName)) {
-            var userName = robot.adapter.client.rtm.dataStore.getDMByName(res.message.user.name);
+            var userObject = robot.adapter.client.rtm.dataStore.getDMByName(res.message.user.name);
             console.log(`sending welcome message to ${userName}`);
             // messages to users are sent like messages to rooms, except they're to a user's id
-            robot.messageRoom(userName.id, WELCOME_MESSAGE);
+            robot.messageRoom(userObject.id, WELCOME_MESSAGE);
         }
     });
 
     robot.respond(/welcome me/i, function(res) {
-        var userName = robot.adapter.client.rtm.dataStore.getDMByName(res.message.user.name);
-        console.log(`responding to 'welcome me' from ${res.message.user.name}`);
-        robot.messageRoom(userName.id, WELCOME_MESSAGE);
+        var userName = res.message.user.name;
+        var userObject = robot.adapter.client.rtm.dataStore.getDMByName(userName);
+        console.log(`responding to 'welcome me' from ${userName}`);
+        robot.messageRoom(userObject.id, WELCOME_MESSAGE);
     });
 };
