f16f0e1b546de7eb452c3e6ec8650c9144b9d721
Topher Brown <t.brown@wordstream.com>
2017-09-07 16:32:31 -0400

    add welcome-bot to channel list

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,11 +1,12 @@
+var ROOM_LIST = ['bot-test', 'welcome-bot'];
+
 module.exports = function(robot) {
     robot.enter(function(res) {
         var roomName = robot.adapter.client.rtm.dataStore.getChannelGroupOrDMById(res.message.room).name;
         console.log(`received enter event. room name: ${roomName}`);
-        if (roomName === 'bot-test') {
-            console.log('replying to new user');
+        if (ROOM_LIST.includes(roomName)) {
             var userName = robot.adapter.client.rtm.dataStore.getDMByName(res.message.user.name);
-            console.log(`sending welcome message to ${username}`);
+            console.log(`sending welcome message to ${userName}`);
             // messages to users are sent like messages to rooms, except they're to a user's id
             res.messageRoom(userName.id, "Welcome to wordstream!");
         }
