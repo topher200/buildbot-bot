cf3dc315f7439d262085a7a9f98e10b3a42698dc
Topher Brown <t.brown@wordstream.com>
2017-09-07 15:51:35 -0400

    retrieve room name (instead of ID)

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,7 +1,8 @@
 module.exports = function(robot) {
     robot.enter(function(res) {
-        console.log(`received enter message: ${res}, ${res.message.room}`);
-        if (res.message.room === 'bot-test') {
+        var roomName = robot.adapter.client.rtm.dataStore.getChannelGroupOrDMById(res.message.room).name;
+        console.log(`received enter message. room name: ${roomName}`);
+        if (roomName === 'bot-test') {
             console.log('replying to new user');
             res.reply("Welcome to wordstream!");
         }
