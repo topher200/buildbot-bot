07361666793610135647e1b5289355483f3a06ba
Topher Brown <t.brown@wordstream.com>
2017-09-07 16:28:53 -0400

    send to user id

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,10 +1,13 @@
 module.exports = function(robot) {
     robot.enter(function(res) {
         var roomName = robot.adapter.client.rtm.dataStore.getChannelGroupOrDMById(res.message.room).name;
-        console.log(`received enter message. room name: ${roomName}`);
+        console.log(`received enter event. room name: ${roomName}`);
         if (roomName === 'bot-test') {
             console.log('replying to new user');
-            res.reply("Welcome to wordstream!");
+            var userName = robot.adapter.client.rtm.dataStore.getDMByName(res.message.user.name);
+            console.log(`sending welcome message to ${username}`);
+            // messages to users are sent like messages to rooms, except they're to a user's id
+            res.messageRoom(userName.id, "Welcome to wordstream!");
         }
     });
 };
